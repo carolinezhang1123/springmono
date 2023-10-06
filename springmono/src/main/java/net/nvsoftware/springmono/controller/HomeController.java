45ef9c3e5d0c2573c3f6a63d19c1db2dce057923
GuGuKing <>
2023-10-05 20:19:01 -0700

    user path variable

--- springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -2,10 +2,7 @@ package net.nvsoftware.springmono.controller;
 
 import net.nvsoftware.springmono.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -15,11 +12,20 @@ public class HomeController {
         return "NVsoftware Home";
     }
 
-    @RequestMapping("/user")
+    @RequestMapping(value = "/user", method= RequestMethod.GET)
     public User user() {
         User user = new User();
         user.setId("3423");
         user.setName("nvso");
+        user.setEmail("sfnlsklfd");
+        return user;
+    }
+
+    @GetMapping("/user/{id}/{username}")
+    public User userByPathVariable(@PathVariable String id, @PathVariable("username") String name) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
         user.setEmail("sfnlsklfd");
         return user;
     }
