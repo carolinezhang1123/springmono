f97edb2e41cffeffa6b0b082b19cac441f54b668
GuGuKing <>
2023-10-05 20:00:49 -0700

    setup actuator

--- springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class HomeController {
-    
+
     @RequestMapping("/")
     public String home() {
         return "NVsoftware Home";
