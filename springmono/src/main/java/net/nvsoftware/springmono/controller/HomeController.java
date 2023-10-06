6c63c83b4d3adf837d5099532326a81b55b02e0b
GuGuKing <>
2023-10-05 20:09:01 -0700

    add user model

--- springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -1,16 +1,26 @@
 package net.nvsoftware.springmono.controller;
 
+import net.nvsoftware.springmono.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
         return "NVsoftware Home";
+    }
+
+    @RequestMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("3423");
+        user.setName("nvso");
+        user.setEmail("sfnlsklfd");
+        return user;
     }
 }
