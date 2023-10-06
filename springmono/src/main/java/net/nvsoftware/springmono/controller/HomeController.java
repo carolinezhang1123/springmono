473a4b60d4732df4991c4d13fd2d64524b67693e
GuGuKing <>
2023-10-05 19:55:09 -0700

    add package controlelr and class Home Controller

---
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -0,0 +1,16 @@
+package net.nvsoftware.springmono.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "NVsoftware Home";
+    }
+}
